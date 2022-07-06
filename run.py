2bb24ea8cfa9cd7ebf0356aa39000e97ae9bdf94
denextden <extenden98@gmail.com>
2022-07-06 21:51:37 +0300

    Заполнил colors, outcome Переделал запрос на заполнение new_animals (безуспешно)

--- run.py
+++ run.py
@@ -19,6 +19,8 @@ query_3 = """
     )
     """
 
+
+
 query_4 = """ 
     CREATE TABLE new_animals (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -36,6 +38,7 @@ query_4 = """
     )   
     """
 
+
 query_5 = """ 
     CREATE TABLE outcome (
         id INTEGER PRIMARY KEY AUTOINCREMENT, 
@@ -44,7 +47,9 @@ query_5 = """
         'month' INTEGER, 
         'year' INTEGER, 
         age_upon_outcome VARCHAR(50),
-        FOREIGN KEY(animal_id) REFERENCES new_animals(id) 
+        new_animals_id INTEGER,
+        FOREIGN KEY(animal_id) REFERENCES new_animals(id), 
+        FOREIGN KEY(new_animals_id) REFERENCES new_animals(id)
     ) 
     """
 
@@ -65,6 +70,7 @@ query_8 = """
         SELECT DISTINCT animal_type FROM animals 
     """
 
+
 query_9 = """
     INSERT INTO outcome (animal_id, subtype, "type", "month", 'year', age_upon_outcome)
         SELECT animal_id, outcome_subtype, outcome_type, outcome_month, outcome_year, age_upon_outcome 
@@ -72,43 +78,42 @@ query_9 = """
     """
 
 query_10 = """
-    INSERT INTO new_animals (animal_id, 'name', date_of_birth)
-        SELECT DISTINCT animal_id, 'name', date_of_birth 
-        FROM animals
+    INSERT INTO new_animals (animal_id, animal_type_id, 'name', breed_id, color_1_id, date_of_birth)
+        SELECT DISTINCT animals.animal_id, animal_type.id, animals.name, breeds.id AS breed, animals.color1, animals.date_of_birth
+    FROM animals
+    JOIN colors
+        ON colors.color = animals.color1
+    LEFT JOIN breeds
+        ON animals.breed = breeds.breed
+    LEFT JOIN animal_type
+        ON animals.animal_type = animal_type.animal_type
+        
     """
 
-query_11 = """
-    INSERT INTO new_animals (animal_type_id)
-        SELECT DISTINCT animal_type.id
-        FROM animal_type
-        JOIN animals ON animals.animal_type = animal_type.animal_type
-        WHERE animals.animal_id = new_animals.animal_id
-    """
-
-query_12 = """
-    INSERT INTO new_animals (breed_id)
-        SELECT DISTINCT breeds.id
-        FROM breeds
-        JOIN animals ON animals.breed = breeds.breed
-        WHERE animals.animal_id = new_animals.animal_id 
-    """
-query_13 = """
-    INSERT INTO new_animals (color_1_id)
-        SELECT DISTINCT colors.id
-        FROM colors
-        JOIN animals ON trim(animals.color1) = colors.color
-        WHERE animals.animal_id = new_animals.animal_id
-    """
-query_14 = """
-    INSERT INTO new_animals (color_2_id)
-        SELECT DISTINCT colors.id
-        FROM colors
-        JOIN animals ON trim(animals.color2) = colors.color
-        WHERE animals.animal_id = new_animals.animal_id
-    """
-
-query_15 = """
-    INSERT INTO new_animals
-        SELECT age_upon_outcome
-        FROM outcome        
-"""
+# query_11 = """
+#     INSERT INTO new_animals (animal_type_id)
+#         SELECT DISTINCT animal_type.id
+#         FROM animal_type
+#         JOIN animals ON animals.animal_type = animal_type.animal_type
+#         WHERE animals.animal_id = new_animals.animal_id
+#     """
+#
+# query_12 = """
+#     INSERT INTO new_animals (breed_id)
+#         SELECT DISTINCT breeds.id
+#         FROM breeds
+#         JOIN animals ON animals.breed = breeds.breed
+#         WHERE animals.animal_id = new_animals.animal_id
+#     """
+# query_13 = """
+#     INSERT INTO new_animals (color_1_id)
+#         SELECT color
+#         FROM colors
+#      """
+# query_14 = """
+#     INSERT INTO new_animals (color_2_id)
+#         SELECT DISTINCT colors.id
+#         FROM colors
+#         JOIN animals ON trim(animals.color2) = colors.color
+#         WHERE animals.animal_id = new_animals.animal_id
+#     """
