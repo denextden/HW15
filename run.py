54b0beced9203a1c84e6c4630a81a3c81ef5d656
denextden <101746817+denextden@users.noreply.github.com>
2022-07-08 20:44:17 +0300

    Update run.py

--- run.py
+++ run.py
@@ -20,7 +20,6 @@ query_3 = """
     """
 
 
-
 query_4 = """ 
     CREATE TABLE new_animals (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -48,8 +47,8 @@ query_5 = """
         'year' INTEGER, 
         age_upon_outcome VARCHAR(50),
         new_animals_id INTEGER,
-        FOREIGN KEY(animal_id) REFERENCES new_animals(id), 
         FOREIGN KEY(new_animals_id) REFERENCES new_animals(id)
+
     ) 
     """
 
@@ -70,16 +69,10 @@ query_8 = """
         SELECT DISTINCT animal_type FROM animals 
     """
 
-
 query_9 = """
-    INSERT INTO outcome (animal_id, subtype, "type", "month", 'year', age_upon_outcome)
-        SELECT animal_id, outcome_subtype, outcome_type, outcome_month, outcome_year, age_upon_outcome 
-        FROM animals 
-    """
-
-query_10 = """
     INSERT INTO new_animals (animal_id, animal_type_id, 'name', breed_id, color_1_id, date_of_birth)
-        SELECT DISTINCT animals.animal_id, animal_type.id, animals.name, breeds.id AS breed, animals.color1, animals.date_of_birth
+        SELECT DISTINCT animals.animal_id, animal_type.id, animals.name, breeds.id AS breed, animals.color1,
+         animals.date_of_birth
     FROM animals
     JOIN colors
         ON colors.color = animals.color1
@@ -87,33 +80,12 @@ query_10 = """
         ON animals.breed = breeds.breed
     LEFT JOIN animal_type
         ON animals.animal_type = animal_type.animal_type
-        
     """
 
-# query_11 = """
-#     INSERT INTO new_animals (animal_type_id)
-#         SELECT DISTINCT animal_type.id
-#         FROM animal_type
-#         JOIN animals ON animals.animal_type = animal_type.animal_type
-#         WHERE animals.animal_id = new_animals.animal_id
-#     """
-#
-# query_12 = """
-#     INSERT INTO new_animals (breed_id)
-#         SELECT DISTINCT breeds.id
-#         FROM breeds
-#         JOIN animals ON animals.breed = breeds.breed
-#         WHERE animals.animal_id = new_animals.animal_id
-#     """
-# query_13 = """
-#     INSERT INTO new_animals (color_1_id)
-#         SELECT color
-#         FROM colors
-#      """
-# query_14 = """
-#     INSERT INTO new_animals (color_2_id)
-#         SELECT DISTINCT colors.id
-#         FROM colors
-#         JOIN animals ON trim(animals.color2) = colors.color
-#         WHERE animals.animal_id = new_animals.animal_id
-#     """
+query_10 = """
+    INSERT INTO outcome (animal_id, subtype, "type", "month", 'year', age_upon_outcome, new_animals_id)
+        SELECT animals.animal_id, outcome_subtype, outcome_type, outcome_month, outcome_year, age_upon_outcome,
+         new_animals.id
+        FROM animals
+        JOIN new_animals ON new_animals.animal_id = animals.animal_id
+    """
